e02f06b695e7607895a515312f42c5bf00d1f668
Alexandre Buisine <alexandrejabuisine@gmail.com>
2016-05-24 20:56:37 +0200

    set version

--- resources/deploy.sh
+++ resources/deploy.sh
@@ -9,6 +9,7 @@ clone_git () {
 	git clone --depth=1 ${GIT_SSH_TARGET} ${CLONE_TEMP_DIR}
 	cd ${CLONE_TEMP_DIR}
 	VERSION=`git rev-parse --short HEAD`
+	sed -i "s/'');/'${VERSION}');/" ${CLONE_TEMP_DIR}/www/version.php
 }
 
 deploy_www () { 
